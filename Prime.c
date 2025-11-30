0ecc9a89770b2cadfe51c9f9b95e53a22d349d3d
adibaluch70 <adibaluch70@eng.rizvi.edu.in>
2025-12-01 00:35:44 +0530

    Create Prime.c

---
+++ Prime.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+/*
+Name:Adi Baluch
+UIN:251P012
+Class: Computer C1
+*/
+int Prime_number(int num){
+if (num <= 1)
+return 0;
+for (int i=2;i*i<num;i++){
+if (num %i == 0)
+return 0;
+}
+return 1;
+}
+int main (){
+int start,end;
+printf("enter the two numbers (start and end):");
+scanf("%d %d",&start,&end);
+printf("prime number between %d and %d are :",start,end);
+for(int i=start;i <= end;i++){
+if(Prime_number(i)){
+printf("%d\n",i);
+}
+}
+return 0;
+}
+
